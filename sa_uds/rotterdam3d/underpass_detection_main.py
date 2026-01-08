b7c0a9c6ce0e801f84d395cf8084618e5ac08fc6
Chaeyeon Moon <chaeyeonmoon.c@gmail.com>
2026-01-08 15:40:35 +0100

    Update underpass_detection_main.py

--- sa_uds/rotterdam3d/underpass_detection_main.py
+++ sa_uds/rotterdam3d/underpass_detection_main.py
@@ -7,6 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description="Detect buildings with underpasses")
 
     parser.add_argument("inputfile", help="Input cityjson file (required)")
+    parser.add_argument("--eps", type=float, default=1e-8, help="Minimum difference between roof and ground areas to consider an underpass")
 
     args = parser.parse_args()
 
@@ -17,24 +18,35 @@ def main():
         print(e)
         sys.exit()
 
+    # 1) Create lists of roofs and grounds per city object
     obj_roofs, roof_bounds = underpass_detection.roof_boundaries(data)
     obj_grounds, ground_bounds = underpass_detection.ground_boundaries(data)
 
-    v_coords = underpass_detection.vertices_idx_to_coords(data)
+    # 2) Translate vertex coordinates from indices
+    v_coords = underpass_detection.vertex_idx_to_coords(data)
 
+    # 3) Get boundary coordinates
     roof_coords = underpass_detection.boundary_idx_to_coords(roof_bounds, v_coords)
     ground_coords = underpass_detection.boundary_idx_to_coords(ground_bounds, v_coords)
 
+    # 4) Generate wkt strings of roof/ground surfaces and output a wkt file for visualization
     ground_wkts = underpass_detection.write_wkt_polygon(ground_coords, 'ground_pre_union.wkt')
     roof_wkts = underpass_detection.write_wkt_polygon(roof_coords, 'roof_pre_union.wkt')
 
-    obj_roof_area = underpass_detection.cal_area(obj_roofs, roof_wkts, 'roof_union.wkt')
-    obj_ground_area = underpass_detection.cal_area(obj_grounds, ground_wkts, 'ground_union.wkt')
+    # 5) Merge roof/ground surfaces and calculate area for each City Object
+    obj_roof_union_wkts, obj_roof_area = underpass_detection.cal_area(obj_roofs, roof_wkts, 'roof_union.wkt')
+    obj_ground_union_wkts, obj_ground_area = underpass_detection.cal_area(obj_grounds, ground_wkts, 'ground_union.wkt')
 
-    underpass_building_uuid, only_roof_building_uuid = underpass_detection.diff_area(obj_roof_area, obj_ground_area)
+    # 6) Calculate the difference between roof and ground area and identify City Objects with underpasses
+    eps = args.eps
+    underpass_obj_ids, only_roof_obj_ids = underpass_detection.diff_area(eps, obj_roof_area, obj_ground_area, obj_roof_union_wkts, obj_ground_union_wkts, f'underpass_obj_eps_{eps}.wkt')
 
-    print(f'underpass_building_uuid\n{underpass_building_uuid}\n')
-    print(f'only_roof_building_uuid\n{only_roof_building_uuid}')
+    print('<City Object IDs with Underpass>')
+    for obj in underpass_obj_ids:
+        print(obj)
+
+    # print(f'\nonly_roof_obj_ids\n{only_roof_obj_ids}')
 
 if __name__ == "__main__":
     main()
+
